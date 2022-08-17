70375e4f85d0885c12c22ef3a1dc3e4e8ea43ddb
RamVemula <111446924+Ram-Vemula@users.noreply.github.com>
2022-08-17 14:55:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ streamlit.header('🍌 Build Your Own Fruit Smoothie')
 #importing PANDAS 
 import pandas as p
 my_fruit_list=p.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+#streamlit.dataframe(my_fruit_list)
 #have to create User Interactive widget so that user can pick the fruits they want in their smoothies
 # Put a pick list 
 streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
