6711a0d65019de38314a9aa06959d2db68741963
RamVemula <111446924+Ram-Vemula@users.noreply.github.com>
2022-08-17 11:58:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit
 streamlit.title('My Parents for New Healthy Diner')
-streamlit.header()
 streamlit.header('Breakfast Menu')
 streamlit.header('Indian')
 streamlit.text('Idly, Dosa, Vada Sambar')
