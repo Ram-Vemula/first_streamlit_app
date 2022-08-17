7ffa311378695a49f9ee04b7c87bacfb06494741
RamVemula <111446924+Ram-Vemula@users.noreply.github.com>
2022-08-17 15:45:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,17 +12,19 @@ my_fruit_list=p.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/
 #streamlit.dataframe(my_fruit_list)
 #have to create User Interactive widget so that user can pick the fruits they want in their smoothies
 # Put a pick list 
-#streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))  
-# Display the table with pick list option 
 # streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index)) --- this takes the column1 as indes which is a number, this doesn't make any sense. have to provide the choice to choose the fruits by names. It is required to change the index to 'Fruit'. 
-
 my_fruit_list=my_fruit_list.set_index('Fruit')
-
+# Display the table with pick list option 
 streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
-#streamlit.dataframe(my_fruit_list)
+
 # We want to filter the table data based on the fruits a customer will choose, so we'll pre-populate the list to set an example for the customer. 
 #let's put a pick list so the customer can pick the fruit what they want
 
 streamlit.dataframe(my_fruit_list)
 
 # we just have to make the table below the picker a bit smarter so it's doesn't load all the fruits, just the ones shown in the pick list
+
+#put the list of selected fruits into a variable 
+fruits_selected=streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show=my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
