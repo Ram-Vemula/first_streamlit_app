20325d24b03012ebeac4c8f12d53525a17c79eff
RamVemula <111446924+Ram-Vemula@users.noreply.github.com>
2022-08-17 14:54:01 -0400

    Update streamlit_app.py
    import PANDAS and read the CSV file into the data frame 'my_fruit_list'

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,12 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('Avacado Toast')
 streamlit.header('🍌 Build Your Own Fruit Smoothie')
+#importing PANDAS 
 import pandas as p
 my_fruit_list=p.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+#have to create User Interactive widget so that user can pick the fruits they want in their smoothies
+# Put a pick list 
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+# Display the table with pick list option 
 streamlit.dataframe(my_fruit_list)
