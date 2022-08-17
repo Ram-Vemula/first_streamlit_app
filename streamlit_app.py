09cf51bc58ec23b209716ba9a93155f0e08c76ad
RamVemula <111446924+Ram-Vemula@users.noreply.github.com>
2022-08-17 13:36:45 -0400

    Update streamlit_app.py
    Changed the menu items and added bf favorite Avacado and toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title('My Parents for New Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favourites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('Avacado Toast')
 streamlit.header('🍌 Build Your Own Fruit Smoothie')
