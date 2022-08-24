70d5dc2bb4c6b264a303319cd6b5d1cb028539cf
RamVemula <111446924+Ram-Vemula@users.noreply.github.com>
2022-08-24 16:43:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,5 +42,9 @@ streamlit.dataframe(fruityvice_normalized)
    
 import snowflake.connector as sfc
 
-
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
