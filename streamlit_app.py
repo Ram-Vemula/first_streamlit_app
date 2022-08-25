352d9cafe00bfeb4f90ead8ddd63cb9f4bad0758
RamVemula <111446924+Ram-Vemula@users.noreply.github.com>
2022-08-25 11:59:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,6 @@ streamlit.dataframe(my_data_rows)
 #adding a text entry box 
 fruit_choice2 = streamlit.text_input('What fruit would you like to add','Jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice2)
-
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 
