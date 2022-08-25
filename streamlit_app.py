036d361d1628aa42a8b47b8005885a9bc5fcc9f8
RamVemula <111446924+Ram-Vemula@users.noreply.github.com>
2022-08-25 15:33:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,7 +85,7 @@ except URLError as e:
 
 #allow the end user to add a fruit to the list # through a funtion
 def insert_row_snowflake(new_fruit):
-  with my_cux.cursor() as my_cur:
+  with my_cnx.cursor() as my_cur:
        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
        return "Thanks for adding " + new_fruit
 fruit_choice2 = streamlit.text_input('What fruit would you like to add?')
