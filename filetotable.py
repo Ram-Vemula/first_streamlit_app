a98016b60546877a1883aae5a1a2eb8adf94ee29
RamVemula <111446924+Ram-Vemula@users.noreply.github.com>
2022-08-29 16:57:20 -0400

    Update filetotable.py

--- filetotable.py
+++ filetotable.py
@@ -1,5 +1,8 @@
 import streamlit as sl
-import pandas as p
+import pandas as pd
 import requests
 import snowflake.connector as sfc
 from urllib.error import URLError
+my_cnx = sfc.connect(**streamlit.secrets["snowflake"])
+read_file = pd.read_excel (r'D:\Avid Data Science Services\AMTPL_Insurance\Actuary\Data\PData.xlsx')
+read_file.to_csv (r'D:\Avid Data Science Services\AMTPL_Insurance\Actuary\Data\PData_Convert.csv', index = None, header=True)
